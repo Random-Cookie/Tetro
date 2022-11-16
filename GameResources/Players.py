9c1fa17355c0edde96f7a279c0f901bd41d72d61
Random-Cookie <sefi100@ntlworld.com>
2022-11-16 00:38:50 +0000

    - Player documentation

--- GameResources/Players.py
+++ GameResources/Players.py
@@ -7,7 +7,15 @@ import GameResources.structure as GR
 
 
 class Player(ABC):
-	def __init__(self, color, initial_pieces: list[GR.Piece]):
+	"""
+	Abstract class to represent a generic player
+	"""
+	def __init__(self, color: str, initial_pieces: list[GR.Piece]):
+		"""
+		Initialise a Player
+		:param color: Color for players pieces
+		:param initial_pieces: Initial Pieces for player
+		"""
 		self.color = color
 		self.pieces = initial_pieces
 		self.has_knocked = False
@@ -16,17 +24,30 @@ class Player(ABC):
 		return self.color
 
 	def print_pieces_names_to_cli(self):
+		"""
+		Print players current hand to the CLI
+		"""
 		print(end='| ')
 		for i in range(len(self.pieces)):
 			print(str(i) + ' : ' + self.pieces[i].name, end=' | ')
 		print()
 
 	def take_turn(self, board: GR.GameBoard):
+		"""
+		Slect then place a piece
+		:param board: The gameboard to analyse
+		"""
 		place_params = self.select_piece(board)
 		while place_params is not None and not self.place_piece(board, place_params):
 			place_params = self.select_piece(board)
 
 	def place_piece(self, board: GR.GameBoard, placement_params: tuple[GR.Piece, int, tuple[int, int]]) -> bool:
+		"""
+		Place a piece
+		:param board:
+		:param placement_params:
+		:return: bool, was placed?
+		"""
 		piece, index, xy = placement_params
 		x, y = xy
 		if board.place_piece(x, y, piece):
@@ -34,7 +55,10 @@ class Player(ABC):
 			return True
 		return False
 
-	def has_won(self):
+	def has_won(self) -> bool:
+		"""
+		Is hand empty
+		"""
 		return len(self.pieces) == 0
 
 	def squares_left(self):
@@ -45,7 +69,12 @@ class Player(ABC):
 		return count
 
 	@abstractmethod
-	def select_piece(self, board) -> tuple[GR.Piece, int, tuple[int, int]]:
+	def select_piece(self, board: GR.GameBoard) -> tuple[GR.Piece, int, tuple[int, int]]:
+		"""
+		Abstract method for selecting a piece all subclasses must implement
+		:param board: The game board for analysis
+		:return: Placement parameters: (piece, piece_index, (x,y))
+		"""
 		return GR.Piece('', [], ''), 0, (0, 0)
 
 
@@ -54,6 +83,11 @@ class HumanPlayer(Player):
 		Player.__init__(self, color, initial_pieces)
 
 	def select_piece(self, board: GR.GameBoard) -> tuple[GR.Piece, int, tuple[int, int]]:
+		"""
+		Display an interface to allow a player to select, manipulate a piece and enter xy coords
+		:param board: Required for printing
+		:return: Placement parameters: (piece, piece_index, (x,y)), None if player knocked
+		"""
 		piece_index_input_string = colored(self.color, self.color) + ' player please select a piece: '
 		command_input_string = '| r: Rotate | r{int}: Rotate x times | f: Flip | k: Knock | {int},{int}: Place ' + \
 								colored('▣ ', self.color) + \
