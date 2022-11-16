6634653fa8a674d6d84908595de34f492af39500
Random-Cookie <sefi100@ntlworld.com>
2022-11-16 23:20:46 +0000

    Doc and skeleton - Score print refactor

--- Driver.py
+++ Driver.py
@@ -5,6 +5,7 @@ from termcolor import colored
 from GameResources.structure import GameBoard
 from GameResources.ObjectFactory import ObjectFactory
 from GameResources.Players import Player, HumanPlayer, RandomPlayer
+from timeit import default_timer as timer
 
 
 class Tetros:
@@ -19,11 +20,13 @@ class Tetros:
                  board_size: tuple[int, int] = (20, 20),
                  initial_pieces: dict = None,
                  players: list[Player] = None,
-                 starting_positions: list[[int, int]] = None):
+                 starting_positions: list[[int, int]] = None,
+                 display_modes: list[str] = None):
         self.initial_pieces = ObjectFactory().generate_shapes() if initial_pieces is None else initial_pieces
         self.players = ObjectFactory.generate_human_players(initial_pieces=initial_pieces) \
             if players is None else players
         self.board = GameBoard((board_size[0], board_size[1]), self.players, starting_positions)
+        self.display_modes = display_modes if display_modes is not None else ['end_pause']
 
     def check_win(self):
         """
@@ -41,30 +44,37 @@ class Tetros:
         Play a game of Tetros
         """
         turns = 0
+        turn_timer = timer()
+        turn_times = []
         while not self.board.is_stalemate(self.players) and not self.check_win():
             turns += 1
             for player in self.players:
+                # TODO Update for take turn return val
                 if player.get_placeables(self.board):
                     player.take_turn(self.board)
                     self.board.update_placeable_lists(self.players)
                 else:
-                    input(colored(player.color, player.color) +
-                          ' skipped as they can''t place a piece. Press enter to skip.')
-            print('Turn ' + str(turns) + ':')
-            self.board.print_to_cli()
-            input('Press enter to continue...')
-        winners = self.check_win()
+                    skip_msg = colored(player.color, player.color) + \
+                          ' skipped as they can''t place a piece.'
+                    if 'pause' in self.display_modes:
+                        input(skip_msg + ' Press enter to skip.')
+                    else:
+                        print(skip_msg)
+
+            if 'pause' in self.display_modes:
+                self.board.print_to_cli()
+                print('Turn ' + str(turns) + ':')
+                input('Press enter to continue...')
+            turn_times.append(timer() - turn_timer)
+            turn_timer = timer()
         self.board.print_to_cli()
-        print('--------------- Final Scores ---------------')
-        if len(winners) > 1:
-            print('The winners were: ' + str(winners).strip('[]'))
-        elif len(winners) == 1:
-            print('The winner was: ' + str(winners[0]))
-        else:
-            pass
-            # TODO work out who won?
         self.print_scores_to_cli()
-        input('Press enter to return ot the main menu...')
+        if 'times' in self.display_modes:
+            print('Turn No   | Time')
+            for i in range(len(turn_times)):
+                print('Turn ' + str(i + 1).ljust(4) + ' | ' + str(round(turn_times[i], 2)))
+        if 'end_pause' in self.display_modes:
+            input('Press enter to return ot the main menu...')
 
     def calculate_player_coverage(self, player: Player) -> tuple[int, int, int, int]:
         """
@@ -147,17 +157,29 @@ class Tetros:
         """
         Print the score to the CLI
         """
+        player_name_length_limit = 11
+        title = ' Final Scores '
+        h_pad = '-' * round((69 + player_name_length_limit - len(title)) / 2)
         scores = self.calculate_player_scores()
-        print('| Player    ', end=' | ')
+        print(h_pad + title + h_pad)
+        print('| ' + 'Player'.ljust(player_name_length_limit), end=' | ')
         for key in scores[list(scores.keys())[0]]:
             print(key.ljust(10), end=' | ')
         print()
         for key in scores.keys():
             score = scores[key]
-            print('| ' + key.color.ljust(10), end=' | ')
+            print('| ' + key.color.ljust(player_name_length_limit), end=' | ')
             for sub_key in score.keys():
                 print(str(score[sub_key]).rjust(10), end=' | ')
             print()
+        winners = self.check_win()
+        if len(winners) > 1:
+            print('The winners were: ' + str(winners).strip('[]'))
+        elif len(winners) == 1:
+            print('The winner was: ' + str(winners[0]))
+        else:
+            pass
+            # TODO work out who won?
 
     @staticmethod
     def get_custom_game_inputs():
