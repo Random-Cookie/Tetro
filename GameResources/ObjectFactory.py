b9bea1cf4fc09e0f61c1999ddcc4316a9300b601
Random-Cookie <sefi100@ntlworld.com>
2022-11-16 03:31:56 +0000

    SPACES
    - convert tabs to spaces

--- GameResources/ObjectFactory.py
+++ GameResources/ObjectFactory.py
@@ -5,114 +5,114 @@ from GameResources.Players import HumanPlayer
 
 @dataclass
 class ObjectFactory:
-	STANDARD_SHAPES = {
-		1: {
-			'coords': [[0, 0]],
-			'name': '1'
-		},
-		2: {
-			'coords': [[0, 0], [0, 1]],
-			'name': '2'
-		},
-		3: {
-			'coords': [[0, 0], [0, 1], [0, 2]],
-			'name': '3I'
-		},
-		4: {
-			'coords': [[0, 0], [0, 1], [1, 1]],
-			'name': '3L'
-		},
-		5: {
-			'coords': [[0, 0], [0, 1], [0, 2], [0, 3]],
-			'name': '4I'
-		},
-		6: {
-			'coords': [[0, 0], [0, 1], [0, 2], [1, 2]],
-			'name': '4l'
-		},
-		7: {
-			'coords': [[0, 0], [1, 0], [1, 1], [2, 0]],
-			'name': '4T'
-		},
-		8: {
-			'coords': [[0, 0], [0, 1], [1, 0], [1, 1]],
-			'name': '4O'
-		},
-		9: {
-			'coords': [[0, 0], [1, 0], [1, 1], [2, 1]],
-			'name': '4Z'
-		},
-		10: {
-			'coords': [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
-			'name': '5I'
-		},
-		11: {
-			'coords': [[0, 0], [0, 1], [0, 2], [0, 3], [1, 3]],
-			'name': '5l'
-		},
-		12: {
-			'coords': [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1]],
-			'name': '5Z'
-		},
-		13: {
-			'coords': [[0, 0], [0, 1], [0, 2], [1, 1], [1, 2]],
-			'name': '5b'
-		},
-		14: {
-			'coords': [[0, 0], [0, 1], [0, 2], [1, 0], [1, 2]],
-			'name': '5C'
-		},
-		15: {
-			'coords': [[0, 0], [0, 1], [0, 2], [0, 3], [1, 1]],
-			'name': '5r'
-		},
-		16: {
-			'coords': [[0, 0], [1, 0], [1, 1], [1, 2], [2, 0]],
-			'name': '5T'
-		},
-		17: {
-			'coords': [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]],
-			'name': '5L'
-		},
-		18: {
-			'coords': [[0, 0], [0, 1], [1, 1], [1, 2], [2, 2]],
-			'name': '5¬'
-		},
-		19: {
-			'coords': [[0, 0], [0, 1], [1, 1], [2, 1], [2, 2]],
-			'name': '5S'
-		},
-		20: {
-			'coords': [[0, 0], [0, 1], [1, 1], [1, 2], [2, 1]],
-			'name': '5#'
-		},
-		21: {
-			'coords': [[0, 1], [1, 0], [1, 1], [1, 2], [2, 1]],
-			'name': '5+'
-		}
-	}
+    STANDARD_SHAPES = {
+        1: {
+            'coords': [[0, 0]],
+            'name': '1'
+        },
+        2: {
+            'coords': [[0, 0], [0, 1]],
+            'name': '2'
+        },
+        3: {
+            'coords': [[0, 0], [0, 1], [0, 2]],
+            'name': '3I'
+        },
+        4: {
+            'coords': [[0, 0], [0, 1], [1, 1]],
+            'name': '3L'
+        },
+        5: {
+            'coords': [[0, 0], [0, 1], [0, 2], [0, 3]],
+            'name': '4I'
+        },
+        6: {
+            'coords': [[0, 0], [0, 1], [0, 2], [1, 2]],
+            'name': '4l'
+        },
+        7: {
+            'coords': [[0, 0], [1, 0], [1, 1], [2, 0]],
+            'name': '4T'
+        },
+        8: {
+            'coords': [[0, 0], [0, 1], [1, 0], [1, 1]],
+            'name': '4O'
+        },
+        9: {
+            'coords': [[0, 0], [1, 0], [1, 1], [2, 1]],
+            'name': '4Z'
+        },
+        10: {
+            'coords': [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
+            'name': '5I'
+        },
+        11: {
+            'coords': [[0, 0], [0, 1], [0, 2], [0, 3], [1, 3]],
+            'name': '5l'
+        },
+        12: {
+            'coords': [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1]],
+            'name': '5Z'
+        },
+        13: {
+            'coords': [[0, 0], [0, 1], [0, 2], [1, 1], [1, 2]],
+            'name': '5b'
+        },
+        14: {
+            'coords': [[0, 0], [0, 1], [0, 2], [1, 0], [1, 2]],
+            'name': '5C'
+        },
+        15: {
+            'coords': [[0, 0], [0, 1], [0, 2], [0, 3], [1, 1]],
+            'name': '5r'
+        },
+        16: {
+            'coords': [[0, 0], [1, 0], [1, 1], [1, 2], [2, 0]],
+            'name': '5T'
+        },
+        17: {
+            'coords': [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]],
+            'name': '5L'
+        },
+        18: {
+            'coords': [[0, 0], [0, 1], [1, 1], [1, 2], [2, 2]],
+            'name': '5¬'
+        },
+        19: {
+            'coords': [[0, 0], [0, 1], [1, 1], [2, 1], [2, 2]],
+            'name': '5S'
+        },
+        20: {
+            'coords': [[0, 0], [0, 1], [1, 1], [1, 2], [2, 1]],
+            'name': '5#'
+        },
+        21: {
+            'coords': [[0, 1], [1, 0], [1, 1], [1, 2], [2, 1]],
+            'name': '5+'
+        }
+    }
 
-	@staticmethod
-	def generate_shapes(player_colors: list = None, shapes: dict = None):
-		ret = []
-		player_colors = ['blue', 'green', 'red', 'yellow'] if player_colors is None else player_colors
-		shapes = ObjectFactory.STANDARD_SHAPES.values() if shapes is None else shapes.values()
-		for color in player_colors:
-			player_shapes = []
-			i = 0
-			for shape in shapes:
-				player_shapes.append(Piece(shape['name'], shape['coords'], color))
-				i += 1
-				if i > 2:
-					break
-			ret.append(player_shapes)
-		return ret
+    @staticmethod
+    def generate_shapes(player_colors: list = None, shapes: dict = None):
+        ret = []
+        player_colors = ['blue', 'green', 'red', 'yellow'] if player_colors is None else player_colors
+        shapes = ObjectFactory.STANDARD_SHAPES.values() if shapes is None else shapes.values()
+        for color in player_colors:
+            player_shapes = []
+            i = 0
+            for shape in shapes:
+                player_shapes.append(Piece(shape['name'], shape['coords'], color))
+                i += 1
+                if i > 2:
+                    break
+            ret.append(player_shapes)
+        return ret
 
-	@staticmethod
-	def generate_human_players(player_colors: list = None, initial_pieces: dict = None):
-		ret = []
-		player_colors = ['blue', 'green', 'red', 'yellow'] if player_colors is None else player_colors
-		initial_pieces = ObjectFactory().generate_shapes() if initial_pieces is None else initial_pieces
-		for i in range(0, len(player_colors)):
-			ret.append(HumanPlayer(player_colors[i], initial_pieces[i]))
-		return ret
+    @staticmethod
+    def generate_human_players(player_colors: list = None, initial_pieces: dict = None):
+        ret = []
+        player_colors = ['blue', 'green', 'red', 'yellow'] if player_colors is None else player_colors
+        initial_pieces = ObjectFactory().generate_shapes() if initial_pieces is None else initial_pieces
+        for i in range(0, len(player_colors)):
+            ret.append(HumanPlayer(player_colors[i], initial_pieces[i]))
+        return ret
--- GameResources/Players.py
+++ GameResources/Players.py
@@ -7,125 +7,125 @@ import GameResources.structure as GR
 
 
 class Player(ABC):
-	"""
-	Abstract class to represent a generic player
-	"""
-	def __init__(self, color: str, initial_pieces: list[GR.Piece]):
-		"""
-		Initialise a Player
-		:param color: Color for players pieces
-		:param initial_pieces: Initial Pieces for player
-		"""
-		self.color = color
-		self.pieces = initial_pieces
-		self.has_knocked = False
+    """
+    Abstract class to represent a generic player
+    """
+    def __init__(self, color: str, initial_pieces: list[GR.Piece]):
+        """
+        Initialise a Player
+        :param color: Color for players pieces
+        :param initial_pieces: Initial Pieces for player
+        """
+        self.color = color
+        self.pieces = initial_pieces
+        self.has_knocked = False
 
-	def __str__(self):
-		return self.color
+    def __str__(self):
+        return self.color
 
-	def print_pieces_names_to_cli(self):
-		"""
-		Print players current hand to the CLI
-		"""
-		print(end='| ')
-		for i in range(len(self.pieces)):
-			print(str(i) + ' : ' + self.pieces[i].name, end=' | ')
-		print()
+    def print_pieces_names_to_cli(self):
+        """
+        Print players current hand to the CLI
+        """
+        print(end='| ')
+        for i in range(len(self.pieces)):
+            print(str(i) + ' : ' + self.pieces[i].name, end=' | ')
+        print()
 
-	def take_turn(self, board: GR.GameBoard):
-		"""
-		Slect then place a piece
-		:param board: The gameboard to analyse
-		"""
-		place_params = self.select_piece(board)
-		while place_params is not None and not self.place_piece(board, place_params):
-			place_params = self.select_piece(board)
+    def take_turn(self, board: GR.GameBoard):
+        """
+        Slect then place a piece
+        :param board: The gameboard to analyse
+        """
+        place_params = self.select_piece(board)
+        while place_params is not None and not self.place_piece(board, place_params):
+            place_params = self.select_piece(board)
 
-	def place_piece(self, board: GR.GameBoard, placement_params: tuple[GR.Piece, int, tuple[int, int]]) -> bool:
-		"""
-		Place a piece
-		:param board:
-		:param placement_params:
-		:return: bool, was placed?
-		"""
-		piece, index, xy = placement_params
-		x, y = xy
-		if board.place_piece(x, y, piece):
-			self.pieces.remove(self.pieces[index])
-			return True
-		return False
+    def place_piece(self, board: GR.GameBoard, placement_params: tuple[GR.Piece, int, tuple[int, int]]) -> bool:
+        """
+        Place a piece
+        :param board:
+        :param placement_params:
+        :return: bool, was placed?
+        """
+        piece, index, xy = placement_params
+        x, y = xy
+        if board.place_piece(x, y, piece):
+            self.pieces.remove(self.pieces[index])
+            return True
+        return False
 
-	def has_won(self) -> bool:
-		"""
-		Is hand empty
-		"""
-		return len(self.pieces) == 0
+    def has_won(self) -> bool:
+        """
+        Is hand empty
+        """
+        return len(self.pieces) == 0
 
-	def squares_left(self):
-		count = 0
-		for piece in self.pieces:
-			for i in range(len(piece.currentCoords)):
-				count += 1
-		return count
+    def squares_left(self):
+        count = 0
+        for piece in self.pieces:
+            for i in range(len(piece.currentCoords)):
+                count += 1
+        return count
 
-	@abstractmethod
-	def select_piece(self, board: GR.GameBoard) -> tuple[GR.Piece, int, tuple[int, int]]:
-		"""
-		Abstract method for selecting a piece all subclasses must implement
-		:param board: The game board for analysis
-		:return: Placement parameters: (piece, piece_index, (x,y))
-		"""
-		return GR.Piece('', [], ''), 0, (0, 0)
+    @abstractmethod
+    def select_piece(self, board: GR.GameBoard) -> tuple[GR.Piece, int, tuple[int, int]]:
+        """
+        Abstract method for selecting a piece all subclasses must implement
+        :param board: The game board for analysis
+        :return: Placement parameters: (piece, piece_index, (x,y))
+        """
+        return GR.Piece('', [], ''), 0, (0, 0)
 
 
 class HumanPlayer(Player):
-	def __init__(self, color, initial_pieces):
-		Player.__init__(self, color, initial_pieces)
+    def __init__(self, color, initial_pieces):
+        Player.__init__(self, color, initial_pieces)
 
-	def __str__(self):
-		return Player.__str__(self)
+    def __str__(self):
+        return Player.__str__(self)
 
-	def select_piece(self, board: GR.GameBoard) -> tuple[GR.Piece, int, tuple[int, int]]:
-		"""
-		Display an interface to allow a player to select, manipulate a piece and enter xy coords
-		:param board: Required for printing
-		:return: Placement parameters: (piece, piece_index, (x,y)), None if player knocked
-		"""
-		piece_index_input_string = colored(self.color, self.color) + ' player please select a piece: '
-		command_input_string = '| r: Rotate | r{int}: Rotate x times | f: Flip | k: Knock | {int},{int}: Place ' + \
-								colored('▣ ', self.color) + \
-								'at (x, y), ' + \
-								colored('▢ ', self.color) + \
-								'Indicates Placeable position |' + \
-								'\n'
-		board.print_to_cli(self)
-		print('Available Pieces:')
-		self.print_pieces_names_to_cli()
-		piece_index = int(input(piece_index_input_string))
-		while not 0 <= piece_index < len(self.pieces):
-			piece_index = int(input(piece_index_input_string))
-		selected_piece = copy.deepcopy(self.pieces[piece_index])
-		board.print_to_cli(self)
-		print('Selected Piece:')
-		selected_piece.print_to_cli()
-		command = input(command_input_string).lower()
-		while re.search('[0-9]+,[0-9]+', command) is None and command != 'k':
-			if command == 'r':
-				selected_piece.rotate()
-			if re.search('r[0-9]+', command) is not None:
-				command = command.strip('r')
-				for i in range(int(command) % 4):
-					selected_piece.rotate()
-			if command == 'f':
-				selected_piece.flip()
-			board.print_to_cli(self)
-			print('Selected Piece:')
-			selected_piece.print_to_cli()
-			command = input(command_input_string).lower()
-		if command == 'k':
-			self.has_knocked = True
-			return None
-		else:
-			x, y = command.split(',')
-			x, y = int(x), int(y)
-			return selected_piece, piece_index, (x, y)
+    def select_piece(self, board: GR.GameBoard) -> tuple[GR.Piece, int, tuple[int, int]]:
+        """
+        Display an interface to allow a player to select, manipulate a piece and enter xy coords
+        :param board: Required for printing
+        :return: Placement parameters: (piece, piece_index, (x,y)), None if player knocked
+        """
+        piece_index_input_string = colored(self.color, self.color) + ' player please select a piece: '
+        command_input_string = '| r: Rotate | r{int}: Rotate x times | f: Flip | k: Knock | {int},{int}: Place ' + \
+                                colored('▣ ', self.color) + \
+                                'at (x, y), ' + \
+                                colored('▢ ', self.color) + \
+                                'Indicates Placeable position |' + \
+                                '\n'
+        board.print_to_cli(self)
+        print('Available Pieces:')
+        self.print_pieces_names_to_cli()
+        piece_index = int(input(piece_index_input_string))
+        while not 0 <= piece_index < len(self.pieces):
+            piece_index = int(input(piece_index_input_string))
+        selected_piece = copy.deepcopy(self.pieces[piece_index])
+        board.print_to_cli(self)
+        print('Selected Piece:')
+        selected_piece.print_to_cli()
+        command = input(command_input_string).lower()
+        while re.search('[0-9]+,[0-9]+', command) is None and command != 'k':
+            if command == 'r':
+                selected_piece.rotate()
+            if re.search('r[0-9]+', command) is not None:
+                command = command.strip('r')
+                for i in range(int(command) % 4):
+                    selected_piece.rotate()
+            if command == 'f':
+                selected_piece.flip()
+            board.print_to_cli(self)
+            print('Selected Piece:')
+            selected_piece.print_to_cli()
+            command = input(command_input_string).lower()
+        if command == 'k':
+            self.has_knocked = True
+            return None
+        else:
+            x, y = command.split(',')
+            x, y = int(x), int(y)
+            return selected_piece, piece_index, (x, y)
