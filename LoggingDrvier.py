0db40ea603eaa4a134eadc63e8ea2b8ec7334c16
Random-Cookie <sefi100@ntlworld.com>
2023-05-28 18:32:05 +0100

    Logging Driver finalisation

--- LoggingDrvier.py
+++ LoggingDrvier.py
@@ -7,30 +7,24 @@ BOARD_SIZE = (20, 20)
 INITIAL_PIECES = ObjectFactory().generate_shapes()
 PLAYERS = [
     ExhaustiveStaticHeatmapPlayer('blue', INITIAL_PIECES[0], BOARD_SIZE, 'file',
-                                  'GameResources/res/heatmaps/bullseye.txt'),
+                                  'GameResources/res/heatmaps/agressiveX.txt'),
     ExhaustiveStaticHeatmapPlayer('green', INITIAL_PIECES[1], BOARD_SIZE, 'file',
-                                  'GameResources/res/heatmaps/bullseye.txt'),
+                                  'GameResources/res/heatmaps/agressiveX.txt'),
     ExhaustiveRandomPlayer('red', INITIAL_PIECES[2]),
-    ExhaustiveRandomPlayer('red', INITIAL_PIECES[3])
+    ExhaustiveRandomPlayer('yellow', INITIAL_PIECES[3])
 ]
-GAME_PARAMS = {
-                           'board_size': BOARD_SIZE,
-                           'players': PLAYERS,
-                           'starting_positions': [[0, 0], [0, 19], [19, 0], [19, 19]],
-                           'initial_pieces': ObjectFactory.generate_shapes()
-                       }, ['end_pause', 'pause', 'skip']
+GAME_PARAMS = {'board_size': BOARD_SIZE,
+               'players': PLAYERS,
+               'starting_positions': [[0, 0], [0, 19], [19, 0], [19, 19]],
+               'initial_pieces': ObjectFactory.generate_shapes()}, ['end_pause', 'times']
 
 GAMES = 50
-
-
-while game_params[1]:
-    if game_params[1]:
-        game_config = game_params[0]
-        if game_config != {}:
-            game = Tetros(game_config['board_size'],
-                          game_config['initial_pieces'],
-                          game_config['players'],
-                          game_config['starting_positions'],
-                          game_params[1])
-            game.play_game()
-    game_params = Tetros.display_main_menu()
+for i in range(GAMES):
+    print('Playing Game ' + str(i))
+    game_config = GAME_PARAMS[0]
+    game = Tetros(game_config['board_size'],
+                  game_config['initial_pieces'],
+                  game_config['players'],
+                  game_config['starting_positions'],
+                  GAME_PARAMS[1])
+    game.play_game()
