3ce219e2d2229f636800ea7bcfb426260421fc86
Random-Cookie <sefi100@ntlworld.com>
2023-05-29 17:58:54 +0100

    refactor

--- Experiments/Utils.py
+++ Experiments/Utils.py
@@ -1,8 +1,7 @@
 import copy
 import json
-import time
-from datetime import datetime
 
+from datetime import datetime
 from GameResources.Game import Tetros
 from GameResources.SimplePlayers import Player
 
@@ -17,39 +16,39 @@ PLAYER_SCORE_TEMPLATE = {
             }
 
 
-def simulate_games(game_params: dict, no_games: int):
+def simulate_games(sim_params: dict, no_games: int):
     logfile_date = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
     total_scores = {}
     total_times = []
-    for player in game_params['players']:
+    for player in sim_params['players']:
         total_scores[player.color] = copy.deepcopy(PLAYER_SCORE_TEMPLATE)
     no_games = no_games if no_games >= 1 else 1
     game = Tetros()
     for i in range(no_games):
         print('Playing Game ' + str(i))
-        game = Tetros(copy.deepcopy(game_params['board_size']),
-                      copy.deepcopy(game_params['initial_pieces']),
-                      copy.deepcopy(game_params['players']),
-                      copy.deepcopy(game_params['starting_positions']),
-                      copy.deepcopy(game_params['display_modes']))
+        game = Tetros(copy.deepcopy(sim_params['board_size']),
+                      copy.deepcopy(sim_params['initial_pieces']),
+                      copy.deepcopy(sim_params['players']),
+                      copy.deepcopy(sim_params['starting_positions']),
+                      copy.deepcopy(sim_params['display_modes']))
         game.play_game()
         # Single Game Logging
-        if game_params['logging_modes']:
+        if sim_params['logging_modes']:
             log_obj = {}
             game_scores = game.calculate_player_scores()
-            if 'game_players' in game_params['logging_modes']:
+            if 'game_players' in sim_params['logging_modes']:
                 log_obj['players'] = make_logable_players(game.players)
-            if 'game_board' in game_params['logging_modes']:
+            if 'game_board' in sim_params['logging_modes']:
                 log_obj['final_board'] = game.board.get_printable_board()
-            if 'game_scores' in game_params['logging_modes']:
+            if 'game_scores' in sim_params['logging_modes']:
                 log_obj['scores'] = game_scores
-            if 'game_times' in game_params['logging_modes']:
+            if 'game_times' in sim_params['logging_modes']:
                 log_obj['turn_times'] = make_loggable_turn_times(game.turn_times)
-            if 'total_scores' in game_params['logging_modes'] or 'average_scores' in game_params['logging_modes']:
+            if 'total_scores' in sim_params['logging_modes'] or 'average_scores' in sim_params['logging_modes']:
                 for player in game.players:
                     for key in game_scores[player.color]:
                         total_scores[player.color][key] += game_scores[player.color][key]
-            if 'total_times' in game_params['logging_modes'] or 'average_times' in game_params['logging_modes']:
+            if 'total_times' in sim_params['logging_modes'] or 'average_times' in sim_params['logging_modes']:
                 game_times = game.turn_times
                 for j in range(len(game_times) - len(total_times)):
                     total_times.append(0)
@@ -60,17 +59,17 @@ def simulate_games(game_params: dict, no_games: int):
                 with open(log_filename, 'w') as write_file:
                     write_file.write(json.dumps(log_obj, indent=4))
     # Aggregate Logging
-    if game_params['logging_modes']:
+    if sim_params['logging_modes']:
         log_obj = {}
-        if 'players' in game_params['logging_modes']:
+        if 'players' in sim_params['logging_modes']:
             log_obj['players'] = make_logable_players(game.players)
-        if 'total_scores' in game_params['logging_modes']:
+        if 'total_scores' in sim_params['logging_modes']:
             log_obj['total_scores'] = total_scores
-        if 'average_scores' in game_params['logging_modes']:
+        if 'average_scores' in sim_params['logging_modes']:
             log_obj['average_scores'] = calculate_average_scores(total_scores, no_games)
-        if 'total_times' in game_params['logging_modes']:
+        if 'total_times' in sim_params['logging_modes']:
             log_obj['total_turn_times'] = make_loggable_turn_times(total_times)
-        if 'average_times' in game_params['logging_modes']:
+        if 'average_times' in sim_params['logging_modes']:
             avg_times = []
             for total_time in total_times:
                 avg_times.append(total_time / no_games)
