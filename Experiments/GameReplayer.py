4229ddad93c97ed271c0bbfb4844106d4dabf976
Random-Cookie <sefi100@ntlworld.com>
2023-05-30 03:18:18 +0100

    Game Replay loop

--- Experiments/GameReplayer.py
+++ Experiments/GameReplayer.py
@@ -1,12 +1,18 @@
 from Experiments.SimUtils import replay_game
 
-
-game_to_replay = input('Input file name of game replay:')
-turn_pause = input('Pause after each turn? (y/n)')
-try:
-    replay_params = ['game_replay']
-    if turn_pause.lower() == 'y':
-        replay_params.append('pause')
-    replay_game('GameReplay*.json', ['game_replay'])
-except Exception as e:
-    print('ERROR' + str(e))
+game_to_replay = input('Input file name of game replay: (e to exit)')
+turn_pause = ''
+if game_to_replay != 'e':
+    turn_pause = input('Pause after each turn? (y/n)')
+while game_to_replay != 'e':
+    try:
+        replay_params = ['game_replay']
+        if turn_pause.lower() == 'y':
+            replay_params.append('pause')
+        replay_game(game_to_replay, replay_params)
+    except Exception as e:
+        print('ERROR' + str(e))
+    finally:
+        game_to_replay = input('Input file name of game replay: (e to exit)\n')
+        if game_to_replay != 'e':
+            turn_pause = input('Pause after each turn? (y/n)')
