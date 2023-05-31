c38c2fa15730bd636ec3d5bd2afb34d919260358
Random-Cookie <sefi100@ntlworld.com>
2023-05-31 04:50:17 +0100

    Game Replay newline fix

--- Experiments/GameReplayer.py
+++ Experiments/GameReplayer.py
@@ -1,9 +1,9 @@
 from Experiments.SimUtils import replay_game
 
-game_to_replay = input('Input file name of game replay: (e to exit)')
+game_to_replay = input('Input file name of game replay: (e to exit)\n')
 turn_pause = ''
 if game_to_replay != 'e':
-    turn_pause = input('Pause after each turn? (y/n)')
+    turn_pause = input('Pause after each turn? (y/n)\n')
 while game_to_replay != 'e':
     try:
         replay_params = ['game_replay']
@@ -15,4 +15,4 @@ while game_to_replay != 'e':
     finally:
         game_to_replay = input('Input file name of game replay: (e to exit)\n')
         if game_to_replay != 'e':
-            turn_pause = input('Pause after each turn? (y/n)')
+            turn_pause = input('Pause after each turn? (y/n)\n')
