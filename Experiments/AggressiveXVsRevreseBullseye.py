8d0d591c44dd867127d3e5e3ac1ddc85738c3d11
Random-Cookie <10joe.cookie@gmail.com>
2023-11-22 03:38:44 +0000

    refactor

--- Experiments/AggressiveXVsRevreseBullseye.py
+++ Experiments/AggressiveXVsRevreseBullseye.py
@@ -5,10 +5,10 @@ from GameResources.AlgorithmicPlayers import ExhaustiveStaticHeatmapPlayer
 BOARD_SIZE = (20, 20)
 INITIAL_PIECES = ObjectFactory().generate_shapes()
 
-PLAYERS = [ExhaustiveStaticHeatmapPlayer('blue', INITIAL_PIECES[0], BOARD_SIZE, 'file', '../GameResources/res/heatmaps/aggressiveX.txt'),
-    ExhaustiveStaticHeatmapPlayer('green', INITIAL_PIECES[1], BOARD_SIZE, 'file', '../GameResources/res/heatmaps/aggressiveX.txt'),
-    ExhaustiveStaticHeatmapPlayer('red', INITIAL_PIECES[2], BOARD_SIZE, 'file', '../GameResources/res/heatmaps/reverse_bullseye.txt'),
-    ExhaustiveStaticHeatmapPlayer('yellow', INITIAL_PIECES[3], BOARD_SIZE, 'file', '../GameResources/res/heatmaps/reverse_bullseye.txt')]
+PLAYERS = [ExhaustiveStaticHeatmapPlayer('blue', INITIAL_PIECES[0], BOARD_SIZE, '../GameResources/res/heatmaps/aggressiveX.txt'),
+    ExhaustiveStaticHeatmapPlayer('green', INITIAL_PIECES[1], BOARD_SIZE, '../GameResources/res/heatmaps/aggressiveX.txt'),
+    ExhaustiveStaticHeatmapPlayer('red', INITIAL_PIECES[2], BOARD_SIZE, '../GameResources/res/heatmaps/reverse_bullseye.txt'),
+    ExhaustiveStaticHeatmapPlayer('yellow', INITIAL_PIECES[3], BOARD_SIZE, '../GameResources/res/heatmaps/reverse_bullseye.txt')]
 
 SIM_PARAMS = {'board_size': BOARD_SIZE,
                'players': PLAYERS,
