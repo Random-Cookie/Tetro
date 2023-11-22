e51f35365d55bae6008e868aa89b711c5f22ad34
Random-Cookie <10joe.cookie@gmail.com>
2023-11-22 19:11:13 +0000

    SimUtils "Release"

--- ConcurrentGameSimulation.py
+++ ConcurrentGameSimulation.py
@@ -1,20 +1,20 @@
-from Experiments.SimUtils import simulate_games
+from GameResources.SimUtils import simulate_concurrent_games
 from GameResources.ObjectFactory import ObjectFactory
-from GameResources.AlgorithmicPlayers import StaticHeatmapPlayer, HeatmapSwitcher
+from GameResources.AlgorithmicPlayers import StaticHeatmapPlayer
 
 BOARD_SIZE = (20, 20)
 INITIAL_PIECES = ObjectFactory().generate_shapes()
 
 PLAYERS = [StaticHeatmapPlayer('blue', INITIAL_PIECES[0], BOARD_SIZE, 'GameResources/res/heatmaps/aggressiveX.txt'),
     StaticHeatmapPlayer('green', INITIAL_PIECES[1], BOARD_SIZE, 'GameResources/res/heatmaps/aggressiveX.txt'),
-    HeatmapSwitcher('red', INITIAL_PIECES[2], BOARD_SIZE),
-    HeatmapSwitcher('yellow', INITIAL_PIECES[3], BOARD_SIZE)]
+    StaticHeatmapPlayer('red', INITIAL_PIECES[2], BOARD_SIZE, 'GameResources/res/heatmaps/reverse_bullseye.txt'),
+    StaticHeatmapPlayer('yellow', INITIAL_PIECES[3], BOARD_SIZE, 'GameResources/res/heatmaps/reverse_bullseye.txt')]
 
-GAME_PARAMS = {'board_size': BOARD_SIZE,
+SIM_PARAMS = {'board_size': BOARD_SIZE,
                'players': PLAYERS,
                'starting_positions': [[0, 0], [0, 19], [19, 0], [19, 19]],
                'initial_pieces': ObjectFactory.generate_shapes(),
-               'display_modes': ['final_board', 'scores', 'times', 'pause', 'skip'],
+               'display_modes': [],
                'logging_modes': ['game_replay']}
 
-simulate_games(GAME_PARAMS, 1)
+simulate_concurrent_games(SIM_PARAMS, 2, 2, 2)
---
+++ SingleGameSimulation.py
@@ -0,0 +1,20 @@
+from GameResources.SimUtils import simulate_games
+from GameResources.ObjectFactory import ObjectFactory
+from GameResources.AlgorithmicPlayers import StaticHeatmapPlayer, HeatmapSwitcher
+
+BOARD_SIZE = (20, 20)
+INITIAL_PIECES = ObjectFactory().generate_shapes()
+
+PLAYERS = [StaticHeatmapPlayer('blue', INITIAL_PIECES[0], BOARD_SIZE, 'GameResources/res/heatmaps/aggressiveX.txt'),
+    StaticHeatmapPlayer('green', INITIAL_PIECES[1], BOARD_SIZE, 'GameResources/res/heatmaps/aggressiveX.txt'),
+    HeatmapSwitcher('red', INITIAL_PIECES[2], BOARD_SIZE),
+    HeatmapSwitcher('yellow', INITIAL_PIECES[3], BOARD_SIZE)]
+
+GAME_PARAMS = {'board_size': BOARD_SIZE,
+               'players': PLAYERS,
+               'starting_positions': [[0, 0], [0, 19], [19, 0], [19, 19]],
+               'initial_pieces': ObjectFactory.generate_shapes(),
+               'display_modes': ['final_board', 'scores', 'times', 'pause', 'skip'],
+               'logging_modes': ['game_replay']}
+
+simulate_games(GAME_PARAMS, 1)
