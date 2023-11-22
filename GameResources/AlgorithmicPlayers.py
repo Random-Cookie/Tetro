799676574ef2e0377f42e22047a3e2f968fd4d98
Random-Cookie <10joe.cookie@gmail.com>
2023-11-22 03:27:03 +0000

    BFSDHM removal

--- GameResources/AlgorithmicPlayers.py
+++ GameResources/AlgorithmicPlayers.py
@@ -166,94 +166,21 @@ class DynamicHeatmapPlayer(StaticHeatmapPlayer):
             for y in range(len(self.current_heatmap[0])):
                 self.current_heatmap[x][y] *= mul
 
-class BigFirstSeekerDHM(DynamicHeatmapPlayer):
-    """
-    Large Piece First Seeking Dynamic Heat Map
-    """
-    def __init__(self, color: str, initial_pieces: list[Piece], board_size: tuple[int, int],
-                 y_off: int = 5, sel_grad: float = (1/3),
-                 adj_weight: int = 1,  diag_weight: int = 2):
         DynamicHeatmapPlayer.__init__(self, color, initial_pieces, board_size)
-        self.y_offset = y_off
-        self.selection_gradient = sel_grad
-        self.adjacent_weight = adj_weight
-        self.diagonal_weight = diag_weight
-        self.turn_count = 0
 
     def update_heatmap(self, board: GameBoard) -> None:
-        """
-        Reset heatmap, then update using adjacency and diagonals
-        :param board: Current Gameboard
-        :return: None
-        """
-        self.current_heatmap = [[0]*self.board_size[1] for i in range(self.board_size[0])]
-        for x in range(len(board.positions)):
-            for y in range(len(board.positions)):
-                for color in board.player_colors:
-                    if board.check_adjacent_squares(x, y, color):
-                        self.current_heatmap[x][y] += self.adjacent_weight
-                    if board.check_diagonal_squares(x, y, color):
-                        self.current_heatmap[x][y] += self.diagonal_weight
-        min_score, max_score = self.heatmap_min_max()
-        if min_score == 0:
-            self.increment_heatmap()
 
     def select_move(self, board: GameBoard) -> Move | None:
         """
-        Update heatmap, cull small pieces using gradient to save processing
-        Score all possible moves, select the best, tie-break if necessary
-        Increment turn count, No moves, knock and
         :param board:
         :return:
         """
-        self.update_heatmap(board)
-        # 5, 1/3
-        min_squares = self.y_offset - (self.selection_gradient * self.turn_count)
-        useable_pieces = []
-        for piece in self.pieces:
-            if len(piece.currentCoords) >= min_squares:
-                useable_pieces.append(piece)
         placeables = self.get_placeables(board)
-        move_scores = {}
         if placeables and not self.has_knocked:
-            for piece in useable_pieces:
-                for location in placeables:
-                    for rotation in [0, 1, 2, 3]:
-                        for flip in [True, False]:
-                            selected_piece = copy.deepcopy(piece)
-                            for i in range(0, rotation):
-                                selected_piece.rotate()
-                            if flip:
-                                piece.flip()
-                            if board.check_piece_fits(location[0],
-                                                      location[1],
-                                                      selected_piece):
-                                ittr_move = Move(selected_piece, self.pieces.index(piece), location)
-                                move_scores[ittr_move] = self.score_move(board, ittr_move)
-            best_score = move_scores[list(move_scores.keys())[0]]
-            best_moves = []
-            for key in move_scores.keys():
-                move_score = move_scores[key]
-                if move_score < best_score:
-                    best_score = move_score
-                    best_moves = [key]
-                elif move_score == best_score:
-                    best_moves.append(key)
-            if len(best_moves) == 1:
-                self.turn_count += 1
-                return best_moves[0]
-            elif len(best_moves) > 1:
-                self.turn_count += 1
-                return self.tiebreak_moves(best_moves)
-            self.turn_count += 1
             self.has_knocked = True
         return None
 
-    @abstractmethod
     def tiebreak_moves(self, moves: list[Move]) -> Move:
         """
-        TODO Default for now
-        :param moves: moves to tie-break
-        :return: A single chosen move
         """
         return StaticHeatmapPlayer.tiebreak_moves(self, moves)
